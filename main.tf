a69520e894a31faf35fb813d57b03e6f72c0021c
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-02-25 12:17:42 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+# --------------------------
+# Create Dynamo DB table
+# --------------------------
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name           = "${var.name_prefix}-terraformstate-locktable"
+  hash_key       = "LockID"
+  read_capacity  = 2
+  write_capacity = 2
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags {
+    Name = "DynamoDB Terraform State Lock Table for ${var.environment}"
+  }
+}
